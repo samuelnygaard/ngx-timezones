40cc7354a426e4a1829dd599dbf63bd69b95691b
Samuel Nygaard <samuelnygaard@users.noreply.github.com>
2020-07-02 23:28:02 +0200

    first version

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  timezone: any;
+  offsetName = 'UTC';
+  altName = 'GMT';
+  tmp;
+  showOffset = true;
+  timezone: string;
+  guessedTimezone: string;
+
+  get code(): string {
+    return `<ngx-timezone [(timezone)]="timezone"></ngx-timezone>`;
+  }
+
+  get code1(): string {
+    return '<button (click)="ngxTimezone.guessedTimezone()">Guess timezone</button>';
+  }
+
+  get code2(): string {
+    return `<ngx-timezone
+    [guess]="true"
+    [showOffset]="${this.showOffset}"
+    offsetName="${this.offsetName}"
+    [(timezone)]="guessedTimezone">
+ </ngx-timezone>`;
+  }
 }
--- projects/ngx-timezone/src/lib/ngx-timezone.component.ts
+++ projects/ngx-timezone/src/lib/ngx-timezone.component.ts
@@ -1,93 +1,185 @@
-import { takeUntil } from 'rxjs/operators';
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
-import { BankGroup, BANKGROUPS } from './data';
+import { countryZones } from './ngx-timezone.data';
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  ChangeDetectorRef,
+} from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { ReplaySubject, Subject } from 'rxjs';
-import * as moment from 'moment-timezone/builds/moment-timezone-with-data';
+import { Observable } from 'rxjs';
+import * as moment from 'moment-timezone';
+import { startWith, map } from 'rxjs/operators';
+
+interface TimezoneGroup {
+  country: string;
+  iso: string;
+  showGroup?: boolean;
+  zones: { zone: string; name: string; offset: string }[];
+}
 
 @Component({
   selector: 'ngx-timezone',
-  templateUrl: './ngx-timezone.component.html',
-  styleUrls: ['./ngx-timezone.component.scss'],
+  template: ` <mat-form-field style="width: 100%"
+    ><mat-select [formControl]="timezoneCtrl" [placeholder]="placeholder">
+      <mat-option>
+        <ngx-mat-select-search
+          [formControl]="searchControl"
+          [placeholderLabel]="searchPlaceholder"
+          [noEntriesFoundLabel]="noResultsLabel"
+        ></ngx-mat-select-search>
+      </mat-option>
+      <ng-template let-option ngFor [ngForOf]="timezoneGroupsOptions | async">
+        <mat-optgroup
+          *ngIf="option.showGroup || option.zones.length > 1; else singleOption"
+          [label]="option.country + ' (' + option.zones.length + ')'"
+        >
+          <mat-option *ngFor="let zone of option.zones" [value]="zone.zone">
+            {{ zone.name }}
+            <span>{{ showOffset ? formatOffset(zone.offset) : '' }}</span>
+          </mat-option>
+        </mat-optgroup>
+        <ng-template #singleOption>
+          <mat-option [value]="option.zones[0].zone">
+            {{ option.zones[0].name }}
+            <span>{{
+              showOffset ? formatOffset(option.zones[0].offset) : ''
+            }}</span>
+          </mat-option>
+        </ng-template>
+      </ng-template>
+    </mat-select>
+  </mat-form-field>`,
 })
-export class NgxTimezoneComponent implements OnInit, OnDestroy {
-  @Input() placeholder = 'Timezone';
-  /** list of banks */
+export class NgxTimezoneComponent implements OnInit {
+  timezoneCtrl = new FormControl();
+  timezoneGroups!: TimezoneGroup[];
+  timezoneGroupsOptions!: Observable<TimezoneGroup[]>;
+  searchControl = new FormControl();
 
-  /** list of bank groups */
-  protected bankGroups: BankGroup[] = BANKGROUPS;
+  @Output() timezoneChange = new EventEmitter<string>();
 
-  /** control for the selected bank for option groups */
-  public bankGroupsCtrl: FormControl = new FormControl();
-
-  /** control for the MatSelect filter keyword for option groups */
-  public bankGroupsFilterCtrl: FormControl = new FormControl();
-
-  /** list of bank groups filtered by search keyword for option groups */
-  public filteredBankGroups: ReplaySubject<BankGroup[]> = new ReplaySubject<
-    BankGroup[]
-  >(1);
-
-  /** Subject that emits when the component has been destroyed. */
-  protected _onDestroy = new Subject<void>();
-
-  constructor() {
-    const m = moment();
-    debugger;
+  @Input() set timezone(timezone: string) {
+    this.timezoneCtrl.setValue(timezone);
   }
 
-  ngOnInit() {
-    // load the initial bank list
-    this.filteredBankGroups.next(this.copyBankGroups(this.bankGroups));
-
-    // listen for search field value changes
-    this.bankGroupsFilterCtrl.valueChanges
-      .pipe(takeUntil(this._onDestroy))
-      .subscribe(() => {
-        this.filterBankGroups();
-      });
-  }
-
-  ngOnDestroy() {
-    this._onDestroy.next();
-    this._onDestroy.complete();
-  }
-
-  protected filterBankGroups() {
-    if (!this.bankGroups) {
-      return;
-    }
-    // get the search keyword
-    let search = this.bankGroupsFilterCtrl.value;
-    const bankGroupsCopy = this.copyBankGroups(this.bankGroups);
-    if (!search) {
-      this.filteredBankGroups.next(bankGroupsCopy);
-      return;
+  @Input() set disabled(value: boolean) {
+    if (value) {
+      this.timezoneCtrl.disable();
     } else {
-      search = search.toLowerCase();
+      this.timezoneCtrl.enable();
     }
-    // filter the banks
-    this.filteredBankGroups.next(
-      bankGroupsCopy.filter((bankGroup) => {
-        const showBankGroup = bankGroup.name.toLowerCase().indexOf(search) > -1;
-        if (!showBankGroup) {
-          bankGroup.banks = bankGroup.banks.filter(
-            (bank) => bank.name.toLowerCase().indexOf(search) > -1
-          );
-        }
-        return bankGroup.banks.length > 0;
-      })
-    );
   }
 
-  protected copyBankGroups(bankGroups: BankGroup[]) {
-    const bankGroupsCopy = [];
-    bankGroups.forEach((bankGroup) => {
-      bankGroupsCopy.push({
-        name: bankGroup.name,
-        banks: bankGroup.banks.slice(),
-      });
+  @Input() guess = false;
+
+  @Input() showOffset = false;
+  @Input() placeholder = 'Select timezone';
+  @Input() searchPlaceholder = 'Search';
+  @Input() noResultsLabel = 'No results';
+  @Input() offsetName = 'UTC';
+
+  constructor(private cdRef: ChangeDetectorRef) {}
+
+  ngOnInit(): void {
+    this.initData();
+    this.timezoneGroupsOptions = this.searchControl.valueChanges.pipe(
+      startWith(''),
+      map((query) => (query ? this.filter(query) : this.timezoneGroups.slice()))
+    );
+    this.timezoneCtrl.valueChanges.subscribe((value) => {
+      this.emitTimezone(value);
     });
-    return bankGroupsCopy;
+
+    if (!this.timezoneCtrl.value && this.guess) {
+      this.guessedTimezone();
+      this.cdRef.detectChanges();
+    }
+  }
+
+  guessedTimezone(): void {
+    const zone = moment.tz.guess();
+    this.timezone = zone;
+    this.emitTimezone(zone);
+  }
+
+  emitTimezone(zone): void {
+    this.timezoneChange.emit(zone);
+  }
+
+  initData(): void {
+    this.timezoneGroups = Object.keys(countryZones).map((iso: string) => {
+      const val = countryZones[iso];
+      const hasMultiple = val.zones.length > 1;
+      const zones = val.zones.map((x) => {
+        return hasMultiple
+          ? {
+              zone: x,
+              name: `${this.formatTimezoneString(x)}`,
+              offset: this.offsetOfTimezone(x),
+            }
+          : {
+              zone: x,
+              name: val.name,
+              offset: this.offsetOfTimezone(x),
+            };
+      });
+      return { iso, country: val.name, zones };
+    });
+  }
+
+  filter(query: string): TimezoneGroup[] {
+    const result: TimezoneGroup[] = [];
+
+    for (const z of this.timezoneGroups) {
+      if (z.country && z.country.toLowerCase().includes(query.toLowerCase())) {
+        result.push(z);
+      } else {
+        const results = z.zones.filter((x) =>
+          x.name.toLowerCase().includes(query.toLowerCase())
+        );
+        if (results.length > 0) {
+          z.zones = results;
+          z.showGroup = z.zones.length > 1;
+          result.push(z);
+        }
+      }
+    }
+
+    return result;
+  }
+
+  formatTimezoneString(zone: string): string {
+    const arr = zone.split('/');
+    return arr[arr.length - 1].split('_').join(' ');
+  }
+
+  offsetOfTimezone(zone: string): string {
+    let offset = moment.tz(zone).utcOffset();
+    const neg = offset < 0;
+    if (neg) {
+      offset = -1 * offset;
+    }
+    const hours = Math.floor(offset / 60);
+    const minutes = (offset / 60 - hours) * 60;
+    return `${neg ? '-' : '+'}${this.rjust(hours.toString(), 2)}:${this.rjust(
+      minutes.toString(),
+      2
+    )}`;
+  }
+
+  formatOffset(offset: string): string {
+    return `(${this.offsetName}${offset})`;
+  }
+
+  private rjust(value: string, width: number, padding = '0'): string {
+    padding = padding || ' ';
+    padding = padding.substr(0, 1);
+    if (value.length < width) {
+      return padding.repeat(width - value.length) + value;
+    } else {
+      return value;
+    }
   }
 }
